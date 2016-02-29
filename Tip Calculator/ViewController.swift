dbeb0fbec938bcd059cbd29effed9abe17382f80
krzychukula <krzychukula@gmail.com>
2016-02-29 10:28:39 +0100

    simple tip calculation logic

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var costTextField: UITextField!
+    @IBOutlet weak var finalCostLabel: UILabel!
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    
+    var mealCost: Float = 0.0
+    var tipPercent: Float = 20.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        costTextField.text = "100"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tipSliderChange(sender: AnyObject) {
+        self.tipPercent = sender.value
+        self.tipPercentLabel.text = "\(self.tipPercent)"
+        
+        self.finalCostLabel.text = "\(finalCost())"
+    }
 
+    @IBAction func costTextFieldChanged(sender: UITextField) {
+        if let cost = Float(sender.text!) {
+            self.mealCost = cost
+        } else {
+            self.mealCost = 0
+        }
+        
+        
+        self.finalCostLabel.text = "\(finalCost())"
+    }
+    
+    func finalCost() -> Float {
+        return mealCost + tipPercent / 100.0 * mealCost
+    }
 }
 
